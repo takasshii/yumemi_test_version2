9b9356b941407e6c77dda917afb454f166a8c612
たかっしー <takashiho0913@gmail.com>
2022-05-03 22:07:31 +0900

    [add] applciationを追加

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/model/TestApplication.kt
@@ -0,0 +1,8 @@
+package jp.co.yumemi.android.code_check.domain.model
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TestApplication: Application() {
+}
