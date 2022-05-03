68a6719e84573527c00c40678d9de513922b3cbb
たかっしー <takashiho0913@gmail.com>
2022-05-04 00:17:34 +0900

    [add] optLongで指定されている文字列の間違えを変更 conut -> count

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModel.kt
@@ -29,7 +29,7 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class OneViewModel @Inject constructor(
-    val repository: IGetResources
+    private val repository: IGetResources
 ) : ViewModel() {
 
     private val _searchInputText: MutableLiveData<String> by lazy {
@@ -68,7 +68,7 @@ class OneViewModel @Inject constructor(
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
+                val forksCount = jsonItem.optLong("forks_count")
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
