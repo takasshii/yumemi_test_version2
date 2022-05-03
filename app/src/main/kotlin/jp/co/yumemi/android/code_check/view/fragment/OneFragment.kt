8ebe45245cae48e38aae5765e19ba893575cc675
たかっしー <takashiho0913@gmail.com>
2022-05-03 21:14:45 +0900

    [fix] インテンド、スペースの変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/OneFragment.kt
@@ -16,33 +16,32 @@ import jp.co.yumemi.android.code_check.databinding.FragmentOneBinding
 import jp.co.yumemi.android.code_check.domain.item.Item
 import jp.co.yumemi.android.code_check.viewModel.OneViewModel
 
-class OneFragment: Fragment(R.layout.fragment_one){
+class OneFragment : Fragment(R.layout.fragment_one) {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
-    {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding= FragmentOneBinding.bind(view)
+        val binding = FragmentOneBinding.bind(view)
 
-        val viewModel= OneViewModel(requireContext())
+        val viewModel = OneViewModel(requireContext())
 
-        val layoutManager= LinearLayoutManager(requireContext())
-        val dividerItemDecoration=
+        val layoutManager = LinearLayoutManager(requireContext())
+        val dividerItemDecoration =
             DividerItemDecoration(requireContext(), layoutManager.orientation)
-        val adapter= CustomAdapter(object : CustomAdapter.OnItemClickListener{
-            override fun itemClick(item: Item){
+        val adapter = CustomAdapter(object : CustomAdapter.OnItemClickListener {
+            override fun itemClick(item: Item) {
                 gotoRepositoryFragment(item)
             }
         })
 
         //EditTextを制御
         binding.searchInputText
-            .setOnEditorActionListener{ editText, action, _ ->
+            .setOnEditorActionListener { editText, action, _ ->
                 //ENTERが押された時の処理
-                if (action== EditorInfo.IME_ACTION_SEARCH){
+                if (action == EditorInfo.IME_ACTION_SEARCH) {
                     editText.text.toString().let {
                         //submitListに入力された文字を代入
-                        viewModel.searchResults(it).apply{
+                        viewModel.searchResults(it).apply {
                             adapter.submitList(this)
                         }
                     }
@@ -52,61 +51,54 @@ class OneFragment: Fragment(R.layout.fragment_one){
             }
 
         //recyclerViewに代入
-        binding.recyclerView.also{
-            it.layoutManager= layoutManager
+        binding.recyclerView.also {
+            it.layoutManager = layoutManager
             it.addItemDecoration(dividerItemDecoration)
-            it.adapter= adapter
+            it.adapter = adapter
         }
     }
 
-    fun gotoRepositoryFragment(item: Item)
-    {
-        val action= OneFragmentDirections
-            .actionRepositoriesFragmentToRepositoryFragment(item= item)
+    fun gotoRepositoryFragment(item: Item) {
+        val action = OneFragmentDirections
+            .actionRepositoriesFragmentToRepositoryFragment(item = item)
         findNavController().navigate(action)
     }
 }
 
 //差分を比較して計算する
 //変更された部分だけ更新できる
-val diff_util= object: DiffUtil.ItemCallback<Item>(){
-    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean
-    {
-        return oldItem.name== newItem.name
+val diff_util = object : DiffUtil.ItemCallback<Item>() {
+    override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem.name == newItem.name
     }
 
-    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean
-    {
-        return oldItem== newItem
+    override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+        return oldItem == newItem
     }
 
 }
 
 class CustomAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<Item, CustomAdapter.ViewHolder>(diff_util){
+) : ListAdapter<Item, CustomAdapter.ViewHolder>(diff_util) {
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-    interface OnItemClickListener{
-    	fun itemClick(item: Item)
+    interface OnItemClickListener {
+        fun itemClick(item: Item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder
-    {
-    	val view= LayoutInflater.from(parent.context)
-            .inflate(R.layout.layout_item, parent, false)
-    	return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int)
-    {
-    	val item= getItem(position)
-        (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text=
-            item.name
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        (holder.itemView.findViewById<View>(R.id.repositoryNameView) as TextView).text = item.name
 
-    	holder.itemView.setOnClickListener{
-     		itemClickListener.itemClick(item)
-    	}
+        holder.itemView.setOnClickListener {
+            itemClickListener.itemClick(item)
+        }
     }
 }
