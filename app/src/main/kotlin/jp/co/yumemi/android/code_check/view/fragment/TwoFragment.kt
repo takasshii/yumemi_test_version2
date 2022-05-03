bf0a198d0f47f6c74d28abd019cb37463a0a0b57
たかっしー <takashiho0913@gmail.com>
2022-05-03 21:04:53 +0900

    [fix] 不必要なセミコロンを取り除いた。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/TwoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/TwoFragment.kt
@@ -30,12 +30,12 @@ class TwoFragment : Fragment(R.layout.fragment_two) {
         //OneFragmentから値を受け取る
         var item = args.item
 
-        _binding.ownerIconView.load(item.ownerIconUrl);
-        _binding.nameView.text = item.name;
-        _binding.languageView.text = item.language;
-        _binding.starsView.text = "${item.stargazersCount} stars";
-        _binding.watchersView.text = "${item.watchersCount} watchers";
-        _binding.forksView.text = "${item.forksCount} forks";
-        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
+        _binding.ownerIconView.load(item.ownerIconUrl)
+        _binding.nameView.text = item.name
+        _binding.languageView.text = item.language
+        _binding.starsView.text = "${item.stargazersCount} stars"
+        _binding.watchersView.text = "${item.watchersCount} watchers"
+        _binding.forksView.text = "${item.forksCount} forks"
+        _binding.openIssuesView.text = "${item.openIssuesCount} open issues"
     }
 }
