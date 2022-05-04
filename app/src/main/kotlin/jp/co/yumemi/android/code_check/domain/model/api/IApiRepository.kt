382105e6b182818025cb45e7270af089b9a0a443
たかっしー <takashiho0913@gmail.com>
2022-05-04 14:30:57 +0900

    [add] API通信の切り出しのため、Module, Repository, Interfaceを作成。

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/model/api/IApiRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/model/api/IApiRepository.kt
@@ -1,7 +1,9 @@
 package jp.co.yumemi.android.code_check.domain.model.api
 
 import io.ktor.client.statement.*
+import kotlinx.coroutines.flow.Flow
 
 interface IApiRepository {
-    suspend fun getStringResources(): HttpResponse
+    //Flow型を返す
+    suspend fun getHttpResponse(inputText: String): Flow<HttpResponse>
 }
--- app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/module/ApiModule.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/domain/module/ApiModule.kt
@@ -1,4 +1,35 @@
 package jp.co.yumemi.android.code_check.domain.module
 
-class ApiModule {
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import jp.co.yumemi.android.code_check.domain.model.api.IApiRepository
+import jp.co.yumemi.android.code_check.infrastracture.api.ApiRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+    @Provides
+    @Singleton
+    //HttpClientのインスタンスを生成する
+    fun provideHttpClient(
+    ): HttpClient {
+        return HttpClient(Android)
+    }
+}
+
+//各リポジトリの結びつけ
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class JsonRepositoryModule {
+    @Singleton
+    @Binds
+    abstract fun bindJsonRepository(
+        impl: ApiRepository
+    ): IApiRepository
 }
