b397ad6288c4e34d8caadc3a9f21329b620155f0
たかっしー <takashiho0913@gmail.com>
2022-05-05 23:36:10 +0900

    [add] 双方向Bindingに変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/view/fragment/OneFragment.kt
@@ -9,11 +9,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,7 +34,7 @@ class OneFragment : Fragment(R.layout.fragment_one) {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentOneBinding.inflate(inflater, container,false)
         binding.oneViewModel= viewModel
-        binding.lifecycleOwner = this
+        binding.lifecycleOwner = viewLifecycleOwner
 
         val layoutManager = LinearLayoutManager(requireContext())
         val dividerItemDecoration =
