9021ccd86a64b90f1b0bd5f04297d2be89158359
たかっしー <takashiho0913@gmail.com>
2022-05-17 16:48:32 +0900

    [add] ViewModelのテストファイルを追加

---
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModelTest.kt
@@ -0,0 +1,28 @@
+package jp.co.yumemi.android.code_check.viewModel
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class OneViewModelTest {
+
+    @Test
+    fun getSearchInputText() {
+    }
+
+    @Test
+    fun getItems() {
+    }
+
+    @Test
+    fun getErrorContent() {
+    }
+
+    @Test
+    fun getLoadingCircle() {
+    }
+
+    @Test
+    fun searchResults() {
+    }
+}
