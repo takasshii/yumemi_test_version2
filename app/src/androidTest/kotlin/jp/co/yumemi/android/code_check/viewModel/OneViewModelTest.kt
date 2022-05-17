7cb09b655c589585e93b62006120db435f7ad286
たかっしー <takashiho0913@gmail.com>
2022-05-17 18:35:14 +0900

    [add] mockkのセットアップ、モックインスタンスの作成

--- app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModelTest.kt
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/viewModel/OneViewModelTest.kt
@@ -1,10 +1,39 @@
 package jp.co.yumemi.android.code_check.viewModel
 
-import org.junit.Assert.*
-
+import jp.co.yumemi.android.code_check.domain.model.api.IApiRepository
+import jp.co.yumemi.android.code_check.domain.model.getResources.IGetResources
+import io.mockk.mockk
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.mockito.Mock
+import io.mockk.coEvery
+import jp.co.yumemi.android.code_check.domain.model.item.Item
+import kotlinx.coroutines.flow.flow
 
+@RunWith(RobolectricTestRunner::class)
 class OneViewModelTest {
+
+    @Mock
+    lateinit var getResourcesRepository: IGetResources
+
+    @Mock
+    lateinit var apiRepository: IApiRepository
+
+    private val mockRepository = mockk<IApiRepository> {
+        coEvery { getHttpResponse(any()) } returns flow {
+            Item(
+                name = "JetBrains/kotlin",
+                ownerIconUrl = "",
+                language = "Koltin",
+                stargazersCount = 38530,
+                watchersCount = 38530,
+                forksCount = 38530,
+                openIssuesCount = 38530
+            )
+        }
+    }
+
 
     @Test
     fun getSearchInputText() {
